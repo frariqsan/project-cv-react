8d3ffd0f87f7f1c9d5bc807955346173a821698e
frariqsan <frariqsan@gmail.com>
2022-06-11 02:20:08 +0200

    FEAT: Added components & CV, FIX: Delete items

---
+++ src/CV/CV.jsx
@@ -0,0 +1,37 @@
+export const CV = {
+    a:{
+        name: "Francisco Javier",
+        lastName: "Riquelme Sánchez",
+        city: "Elche/Alicante",
+        email: "frariqsan@gmail.com",
+        birthDate: "10/12/1996",
+        phone: "(+34) 123456789",
+        image: "",
+        gitHub: "https://github.com/frariqsan",
+        about:[
+            {
+                info: "Trabajo bien en equipo",
+            },
+            {
+                info: "Posibilidad de trabajo en remoto como presencial",
+            },
+            {
+                info: "Me gusta tener recursos y ejecutarlos en el momento exacto",
+            },
+        ],
+    },
+    education: [
+        {
+            name: "Bootcamp FullStack Developer",
+            date: "May.2022 - Jul.2022",
+            where: "IES Severo Ochoa",
+        },
+        {
+            name: "Grado Medio Sistemas Microinformaticos y Redes",
+            date: "2015 - 2018",
+            where: "IES Severo Ochoa",
+        },
+    ]
+}
+
+export default CV;
